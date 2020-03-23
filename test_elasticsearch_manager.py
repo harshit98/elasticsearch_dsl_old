5c66b075d2a044d2f5ae00769b13cd46d8f35c9c
harshit98 <harshitprasad28@gmail.com>
2020-03-23 13:51:38 +0530

    Maintain only old version of elasticsearch-dsl i.e 2.4.0

--- test_elasticsearch_manager.py
+++ test_elasticsearch_manager.py
@@ -1,6 +1,5 @@
-from elasticsearch2_dsl_repo import VERSION as es2_version
-from elasticsearch7_dsl_repo import VERSION as es7_version
+from elasticsearch_dsl_old import Search, query
+from elasticsearch_dsl_old import VERSION
 
-print("Elasticsearch 2: ", es2_version)
-print("Elasticsearch 7: ", es7_version)
+print("ElasticSearch Version: ", VERSION)
 
